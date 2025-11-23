8114edb88815378a8620a44d15448cac5abcdc4f
qkrgusdn556 <qkrgusdn556@gmail.com>
2025-11-24 04:49:59 +0900

    Update server.js for Render deployment

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ const path = require("path");
 const connectDB = require("./db/connect");
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+
 
 // ⭐ JSON 파싱
 app.use(express.json());
